ce3df01a82500f93dfeb1d640a8eed3269c00e65
lium <ming.liu@nbhmit.com>
2015-11-04 15:43:13 +0800

    search vo added

---
+++ src/com/vos/DeviceSearchVo.java
@@ -0,0 +1,26 @@
+package com.vos;
+
+public class DeviceSearchVo {
+
+	private String brand;
+	private String bStartDate;
+	private String bEndDate;
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getbStartDate() {
+		return bStartDate;
+	}
+	public void setbStartDate(String bStartDate) {
+		this.bStartDate = bStartDate;
+	}
+	public String getbEndDate() {
+		return bEndDate;
+	}
+	public void setbEndDate(String bEndDate) {
+		this.bEndDate = bEndDate;
+	}
+}
